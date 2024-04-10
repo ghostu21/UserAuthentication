c98645ee7e031536817d71774e083350b4459042
Sahhil Lahoti <90179363+ghostu21@users.noreply.github.com>
2024-04-10 22:09:57 +0530

    Initialize the Project using Spring  initializrIn

---
+++ src/main/java/com/mayank/UserAuthenication/UserAuthenicationApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.UserAuthenication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenicationApplication.class, args);
+	}
+
+}
